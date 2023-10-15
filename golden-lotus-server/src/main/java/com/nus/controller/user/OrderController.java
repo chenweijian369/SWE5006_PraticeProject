159b07fd66e68276d93ab5a6a39fa2205552cb9e
chenweijian369 <e1221497@u.nus.edu>
2023-10-15 20:55:10 +0800

    add order controller

---
+++ golden-lotus-server/src/main/java/com/nus/controller/user/OrderController.java
@@ -0,0 +1,11 @@
+package com.nus.controller.user;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/order")
+@Api(tags = "Order Relevent Interface")
+public class OrderController {
+}
