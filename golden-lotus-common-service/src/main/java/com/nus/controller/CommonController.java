770a0d26afcc345fb1f5cfda390736f120c02f12
chenweijian369 <e1221497@u.nus.edu>
2023-10-30 21:46:51 +0800

    打完了jar包

--- golden-lotus-common-service/src/main/java/com/nus/controller/CommonController.java
+++ golden-lotus-common-service/src/main/java/com/nus/controller/CommonController.java
@@ -11,10 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +24,7 @@ import java.util.Map;
 @RequestMapping("/common")
 @Api(tags = "Common Relevant Interface")
 @Slf4j
+@CrossOrigin
 public class CommonController {
 
     @Autowired
